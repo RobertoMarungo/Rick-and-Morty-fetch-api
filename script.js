ace59a95acdfd19045dab04eb9087960e40ac126
Roberto Marungo <robmarungo.dev@gmail.com>
2023-05-13 17:54:58 -0500

    Added card using innerHTML to render card from API Data Object

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const toggleMode = () => {};
 const renderCharacter = () => {
     let card = document.createElement('li');
     card.className = 'card';
+    card.innerHTML = `
+    <img src="${character.image}">
+    <div class="content">
+      <h4>${character.name}</h4>
+      <p><em>Gender:</em> ${character.gender}</P>
+      <p>Origin: ${character.origin.name}</P>
+      <p>Location: ${character.location.name}</P>
+      <p>${character.species} - ${character.status}</P>
+    </div>`;
 };
 
 const getCharacters = () => {
