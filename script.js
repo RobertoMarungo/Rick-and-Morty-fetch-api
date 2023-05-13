8f7265a6676522ce73d5a90922e7a7356e28c27a
Roberto Marungo <robmarungo.dev@gmail.com>
2023-05-13 17:39:16 -0500

    Added while loop to getCharacter

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const toggleMode = () => {};
 
 const renderCharacter = () => {};
 
-const getCharacters = () => {};
+const getCharacters = () => {
+    const numOfPages = 20;
+    let page = 0;
+
+    while (page <= numOfPages) {
+        page++;
+        fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+            .then(res => res.json())
+            .then(data => console.log(data));
+    }
+};
 
 const init = () => {
     getCharacters();
